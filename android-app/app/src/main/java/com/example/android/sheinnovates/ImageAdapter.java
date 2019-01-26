37921e21f6596782db86735b2f7d47d5534590aa
Kimberly Lo <32209256+kimberll@users.noreply.github.com>
2019-01-26 11:05:40 -0500

    Commit android app

---
+++ android-app/app/src/main/java/com/example/android/sheinnovates/ImageAdapter.java
@@ -0,0 +1,60 @@
+package com.example.android.sheinnovates;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new ViewGroup.LayoutParams(365, 365));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(0, 0, 0, 0);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+
+    // references to our images
+    private Integer[] mThumbIds = {
+            R.drawable.test, R.drawable.test1,
+            R.drawable.test2, R.drawable.test3,
+            R.drawable.test4, R.drawable.test5,
+            R.drawable.test, R.drawable.test1,
+            R.drawable.test2, R.drawable.test3,
+            R.drawable.test4, R.drawable.test5,
+            R.drawable.test, R.drawable.test1,
+            R.drawable.test2, R.drawable.test3,
+            R.drawable.test4, R.drawable.test5,
+            R.drawable.test, R.drawable.test1,
+            R.drawable.test2, R.drawable.test3,
+            R.drawable.test4, R.drawable.test5
+    };
+}
---
+++ android-app/app/src/main/java/com/example/android/sheinnovates/MainActivity.java
@@ -0,0 +1,141 @@
+package com.example.android.sheinnovates;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mTopToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(MainActivity.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mTopToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(mTopToolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_favorite) {
+            Toast.makeText(MainActivity.this, "Action clicked", Toast.LENGTH_LONG).show();
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+            String[] options = new String[]{
+                    "OPTION 1",
+                    "OPTION 2",
+                    "OPTION 3",
+                    "OPTION 4",
+                    "OPTION 5"
+            };
+
+            final boolean[] checkedOptions = new boolean[]{
+                    false, // 1
+                    false, // 2
+                    false, // 3
+                    false, // 4
+                    false // 5
+
+            };
+
+            final List<String> optionsList = Arrays.asList(options);
+
+            // Set multiple choice items for alert dialog
+                /*
+                    AlertDialog.Builder setMultiChoiceItems(CharSequence[] items, boolean[]
+                    checkedItems, DialogInterface.OnMultiChoiceClickListener listener)
+                        Set a list of items to be displayed in the dialog as the content,
+                        you will be notified of the selected item via the supplied listener.
+                 */
+                /*
+                    DialogInterface.OnMultiChoiceClickListener
+                    public abstract void onClick (DialogInterface dialog, int which, boolean isChecked)
+
+                        This method will be invoked when an item in the dialog is clicked.
+
+                        Parameters
+                        dialog The dialog where the selection was made.
+                        which The position of the item in the list that was clicked.
+                        isChecked True if the click checked the item, else false.
+                 */
+            builder.setMultiChoiceItems(options, checkedOptions, new DialogInterface.OnMultiChoiceClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+
+                    checkedOptions[which] = isChecked;
+
+                    String currentItem = optionsList.get(which);
+
+                    Toast.makeText(getApplicationContext(),
+                            currentItem + " " + isChecked, Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            builder.setCancelable(false);
+
+            builder.setTitle("What are you interested in?");
+
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // Notify the current action
+                    Toast.makeText(getApplicationContext(),
+                            "Okay!", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            builder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // Do something when click the neutral button
+                }
+            });
+
+            AlertDialog dialog = builder.create();
+            // Display alert dialog
+            dialog.show();
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
